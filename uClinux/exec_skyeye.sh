38fb9fd35390e9cc7ad8af4c6d936c68f84dd0a7
Michael.Kang <blackfin.kang@gmail.com>
2010-06-13 15:36:03 +0800

    change the uart mod from term to stdio
    For we can auto test all the testcase and display
    uart output in the same terminal with skyeye. Change
    the uart mod option from term to stdio in all the skyeye.conf
    files.

--- uClinux/exec_skyeye.sh
+++ uClinux/exec_skyeye.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 cd $1
 skyeye_path=$2
-test -e linux &&	${skyeye_path}/skyeye -e linux -c skyeye.conf
-test -e vmlinux &&	${skyeye_path}/skyeye -e vmlinux -c skyeye.conf
-test -e image.elf &&  ${skyeye_path}/skyeye -e image.elf -c skyeye.conf 
-test -e image.rom &&  ${skyeye_path}/skyeye -c skyeye.conf
+test -e linux &&	${skyeye_path}/skyeye -n -e linux -c skyeye.conf
+test -e vmlinux &&	${skyeye_path}/skyeye -n -e vmlinux -c skyeye.conf
+test -e image.elf &&  ${skyeye_path}/skyeye -n -e image.elf -c skyeye.conf 
+test -e image.rom &&  ${skyeye_path}/skyeye -n -c skyeye.conf
 
