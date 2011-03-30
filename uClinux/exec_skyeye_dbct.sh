f1552d34fb00d36fdc8f0e4c571a615662426cd5
yuanzheng <tsinghuayuan86@gmail.com>
2011-03-30 13:45:05 +0800

    Modify skyeye to skyeye_main.py in autotest scripts.
    Modify skyeye to skyeye_main.py in autotest scripts.
    
    Signed-off-by: yuanzheng <tsinghuayuan86@gmail.com>

--- uClinux/exec_skyeye_dbct.sh
+++ uClinux/exec_skyeye_dbct.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 cd $1
 skyeye_path=$2
-test -e linux &&	${skyeye_path}/skyeye -e linux -c skyeye_dbct.conf
-test -e vmlinux &&	${skyeye_path}/skyeye -e vmlinux -c skyeye_dbct.conf
-test -e vmlinux.large &&  ${skyeye_path}/skyeye -e  vmlinux.large -c skyeye_dbct.conf 
-test -e vmlinux-8-332 &&  ${skyeye_path}/skyeye -e  vmlinux-8-332 -c skyeye_dbct.conf
+test -e linux &&	${skyeye_path}/skyeye_main.py -e linux -c skyeye_dbct.conf
+test -e vmlinux &&	${skyeye_path}/skyeye_main.py -e vmlinux -c skyeye_dbct.conf
+test -e vmlinux.large &&  ${skyeye_path}/skyeye_main.py -e  vmlinux.large -c skyeye_dbct.conf 
+test -e vmlinux-8-332 &&  ${skyeye_path}/skyeye_main.py -e  vmlinux-8-332 -c skyeye_dbct.conf
